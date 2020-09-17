0fc6e7ffc7e78c1bbaf2a5006d071ff1d0d1fb2c
vlads <vlads4377@gmail.com>
2020-09-16 23:04:49 -0700

    Implement Cost Function Reg

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -19,7 +19,16 @@ grad = zeros(size(theta));
 
 
 
+sigmoid = sigmoid(X * theta);
 
+J = 1 / m * sum(-y.*log(sigmoid) - (1 - y).* log(1 - sigmoid)) + (lambda / (2 * m)) * sum(theta(2:end).^2);
+
+
+grad(1) = 1 / m * sum((sigmoid - y) .* X(:, 1))
+
+for i = 2:size(theta, 1)
+	grad(i) = 1 / m * sum((sigmoid - y) .* X(:, i)) + lambda / m * (theta(i));
+end
 
 
 % =============================================================
